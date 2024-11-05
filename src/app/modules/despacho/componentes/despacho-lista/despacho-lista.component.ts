d8a4ebb8153f68eed8dcbb789a3597bf227830d6
sebastian <sebastian.h.piedrahita@gmail.com>
2024-11-05 16:09:26 -0500

    feat: despachos vista

--- src/app/modules/despacho/componentes/despacho-lista/despacho-lista.component.ts
+++ src/app/modules/despacho/componentes/despacho-lista/despacho-lista.component.ts
@@ -1,14 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { General } from '../../../../common/clases/general';
+import { mapeo } from '../../../../common/mapeos/documentos';
+import { DespachoService } from '../../servicios/despacho.service';
+import { Observable } from 'rxjs';
+import { ButtonComponent } from "../../../../common/components/ui/button/button.component";
+import { TablaComunComponent } from "../../../../common/components/ui/tablas/tabla-comun/tabla-comun.component";
 
 @Component({
   selector: 'app-despacho-lista',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `<p>despacho-lista works!</p>`,
+  imports: [CommonModule, ButtonComponent, TablaComunComponent],
+  templateUrl: './despacho-lista.component.html',
   styleUrl: './despacho-lista.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DespachoListaComponent { }
+export default class DespachoListaComponent extends General implements OnInit {
+  private _despachoService = inject(DespachoService);
+  public mapeoDocumento = mapeo;
+  public despachos$: Observable<any[]>;
+  public arrParametrosConsulta: any = {
+    filtros: [],
+    limite: 50,
+    desplazar: 0,
+    ordenamientos: [],
+    limite_conteo: 10000,
+    modelo: 'RutDespacho',
+  };
+
+  ngOnInit() {
+    this.consultaLista();
+  }
+
+  consultaLista() {
+    this.despachos$ = this._despachoService.lista(this.arrParametrosConsulta);
+  }
+}
