090a513092ea0e7330743be85bf893467c06de9c
sebas957 <baxtian957@hotmail.com>
2024-11-06 10:10:04 -0500

    feat: Formulario vehiculo

--- src/app/modules/vehiculo/componentes/vehiculo-formulario/vehiculo-formulario.component.ts
+++ src/app/modules/vehiculo/componentes/vehiculo-formulario/vehiculo-formulario.component.ts
@@ -1,14 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { General } from '../../../../common/clases/general';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonComponent } from "../../../../common/components/ui/button/button.component";
+import { RouterLink } from '@angular/router';
+import { InputComponent } from "../../../../common/components/ui/form/input/input.component";
+import { SwitchComponent } from "../../../../common/components/ui/form/switch/switch.component";
 
 @Component({
   selector: 'app-vehiculo-formulario',
   standalone: true,
   imports: [
     CommonModule,
-  ],
-  template: `<p>vehiculo-formulario works!</p>`,
+    ButtonComponent,
+    ReactiveFormsModule,
+    RouterLink,
+    InputComponent,
+    SwitchComponent
+],
+  templateUrl: './vehiculo-formulario.component.html',
   styleUrl: './vehiculo-formulario.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class VehiculoFormularioComponent { }
+export default class VehiculoFormularioComponent extends General { 
+
+  @Input() informacionVehiculo: any;
+  @Output() dataFormulario: EventEmitter<any> = new EventEmitter();
+
+
+  public formularioVehiculo= new FormGroup({
+    placa: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.maxLength(6),
+      ])
+    ),
+    capacidad: new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ])
+    ),
+    estado_activo: new FormControl(true),
+  });
+
+  enviar() {
+    if (this.formularioVehiculo.valid) {
+      return this.dataFormulario.emit(this.formularioVehiculo.value);
+    } else {
+      this.formularioVehiculo.markAllAsTouched();
+    }
+  }
+
+}
--- src/app/modules/vehiculo/componentes/vehiculo-nuevo/vehiculo-nuevo.component.ts
+++ src/app/modules/vehiculo/componentes/vehiculo-nuevo/vehiculo-nuevo.component.ts
@@ -1,14 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { General } from '../../../../common/clases/general';
+import { VehiculoService } from '../../servicios/vehiculo.service';
+import VehiculoFormularioComponent from "../vehiculo-formulario/vehiculo-formulario.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-vehiculo-nuevo',
   standalone: true,
   imports: [
     CommonModule,
-  ],
-  template: `<p>vehiculo-nuevo works!</p>`,
+    VehiculoFormularioComponent,
+    RouterLink
+],
+  templateUrl: './vehiculo-nuevo.component.html',
   styleUrl: './vehiculo-nuevo.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class VehiculoNuevoComponent { }
+export default class VehiculoNuevoComponent extends General { 
+
+  private vehiculoService = inject(VehiculoService);
+
+  informacionVehiculo: any = {
+    placa: "",
+    capacidad: "",
+    estado_activo: ""
+  };
+
+  enviarFormulario(formulario: any) {
+    this.vehiculoService.guardarVehiculo(formulario).subscribe((respuesta: any) => {
+      this.alerta.mensajaExitoso('Se ha creado el vehículo exitosamente.')
+      this.router.navigate(['/admin/vehiculo/lista']);
+    });
+  }
+
+}
