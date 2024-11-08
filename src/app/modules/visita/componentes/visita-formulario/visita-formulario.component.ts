319aa235bd2c89478ca8789305c6a0664b784372
sebas957 <baxtian957@hotmail.com>
2024-11-08 15:23:29 -0500

    fix: mapas

--- src/app/modules/visita/componentes/visita-formulario/visita-formulario.component.ts
+++ src/app/modules/visita/componentes/visita-formulario/visita-formulario.component.ts
@@ -1,14 +1,78 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { General } from '../../../../common/clases/general';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../../../common/components/ui/button/button.component';
+import { RouterLink } from '@angular/router';
+import { InputComponent } from '../../../../common/components/ui/form/input/input.component';
+import { SwitchComponent } from '../../../../common/components/ui/form/switch/switch.component';
 
 @Component({
   selector: 'app-visita-formulario',
   standalone: true,
   imports: [
     CommonModule,
+    ButtonComponent,
+    ReactiveFormsModule,
+    RouterLink,
+    InputComponent,
+    SwitchComponent,
   ],
   template: `<p>visita-formulario works!</p>`,
   styleUrl: './visita-formulario.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class VisitaFormularioComponent { }
+export default class VisitaFormularioComponent
+  extends General
+  implements OnInit
+{
+  @Input() informacionVisita: any;
+  @Input({ required: true }) formularioTipo: 'editar' | 'crear';
+  @Output() dataFormulario: EventEmitter<any> = new EventEmitter();
+
+  public formularioVisita = new FormGroup({
+    fecha: new FormControl(''),
+    documento: new FormControl(''),
+    destinatario: new FormControl(''),
+    destinatario_direccion: new FormControl(''),
+    destinatario_telefono: new FormControl(''),
+    destinatario_correo: new FormControl(''),
+    peso: new FormControl(''),
+    volumen: new FormControl(''),
+    latitud: new FormControl(''),
+    longitud: new FormControl(''),
+    decodificado: new FormControl(false),
+  });
+
+  ngOnInit(): void {
+    if (this.formularioTipo === 'editar') {
+      this.formularioVisita.patchValue({
+        fecha: this.informacionVisita.fecha,
+        documento: this.informacionVisita.documento,
+        destinatario: this.informacionVisita.destinatario,
+        destinatario_direccion: this.informacionVisita.destinatario_direccion,
+        destinatario_telefono: this.informacionVisita.destinatario_telefono,
+      });
+    }
+  }
+
+  enviar() {
+    if (this.formularioVisita.valid) {
+      return this.dataFormulario.emit(this.formularioVisita.value);
+    } else {
+      this.formularioVisita.markAllAsTouched();
+    }
+  }
+}
--- src/app/modules/visita/componentes/visita-nuevo/visita-nuevo.component.ts
+++ src/app/modules/visita/componentes/visita-nuevo/visita-nuevo.component.ts
@@ -1,14 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { General } from '../../../../common/clases/general';
+import { VisitaService } from '../../servicios/visita.service';
+import VisitaFormularioComponent from "../visita-formulario/visita-formulario.component";
 
 @Component({
   selector: 'app-visita-nuevo',
   standalone: true,
   imports: [
     CommonModule,
-  ],
-  template: `<p>visita-nuevo works!</p>`,
+    VisitaFormularioComponent
+],
+  templateUrl: './visita-nuevo.component.html',
   styleUrl: './visita-nuevo.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class VisitaNuevoComponent { }
+export default class VisitaNuevoComponent extends General implements OnInit {
+
+  private guiaService = inject(VisitaService);
+
+  informacionVisita: any = {
+    fecha: '',
+    documento: '',
+    destinatario: '',
+    destinatario_direccion: '',
+    destinatario_correo: '',
+    peso: '',
+    volumen: '',
+    latitud: '',
+    longitud: '',
+    decodificado: ''
+  };
+
+  ngOnInit() {}
+
+  enviarFormulario(formulario: any) {
+    this.guiaService.guardarGuias(formulario).subscribe((respuesta: any) => {
+      this.alerta.mensajaExitoso('Se ha creado el contacto exitosamente.')
+      this.router.navigate(['/admin/visita/lista']);
+    });
+  }
+
+ }
