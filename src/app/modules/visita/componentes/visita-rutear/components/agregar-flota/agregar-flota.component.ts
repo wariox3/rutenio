e3483e669a1d68cabb755f7d17700b903286d270
sebastian <sebastian.h.piedrahita@gmail.com>
2024-11-13 16:52:42 -0500

    feat: agregar flota

--- src/app/modules/visita/componentes/visita-rutear/components/agregar-flota/agregar-flota.component.ts
+++ src/app/modules/visita/componentes/visita-rutear/components/agregar-flota/agregar-flota.component.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   inject,
   Input,
   OnInit,
+  Output,
 } from '@angular/core';
 import {
   FormArray,
@@ -16,11 +18,19 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { LabelComponent } from '../../../../../../common/components/ui/form/label/label.component';
 import { VehiculoService } from '../../../../../vehiculo/servicios/vehiculo.service';
 import { General } from '../../../../../../common/clases/general';
-import { map, Observable } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  switchMap,
+} from 'rxjs';
 import { ListaVehiculo } from '../../../../../../interfaces/vehiculo/vehiculo.interface';
 import { ParametrosConsulta } from '../../../../../../interfaces/general/api.interface';
 import { ButtonComponent } from '../../../../../../common/components/ui/button/button.component';
 import { FlotaService } from '../../../../../flota/servicios/flota.service';
+import { KTModal } from '../../../../../../../metronic/core';
 
 @Component({
   selector: 'app-agregar-flota',
@@ -38,9 +48,12 @@ import { FlotaService } from '../../../../../flota/servicios/flota.service';
 })
 export class AgregarFlotaComponent extends General implements OnInit {
   @Input() itemsSeleccionados: number[] = [];
+  @Output() emitirConsultarLista: EventEmitter<void>;
   private _vehiculoService = inject(VehiculoService);
   private _flotaService = inject(FlotaService);
   public vehiculosDisponibles$: Observable<ListaVehiculo[]>;
+  public textoBusqueda = new FormControl();
+  private _vehiculosIds: number[] = [];
   private _parametrosConsulta: ParametrosConsulta = {
     filtros: [],
     limite: 50,
@@ -57,6 +70,7 @@ export class AgregarFlotaComponent extends General implements OnInit {
   constructor() {
     super();
     this.vehiculosDisponibles$ = new Observable();
+    this.emitirConsultarLista = new EventEmitter();
   }
 
   ngOnInit(): void {
@@ -64,6 +78,64 @@ export class AgregarFlotaComponent extends General implements OnInit {
     this.formularioFlota.patchValue({
       flota: this.itemsSeleccionados,
     });
+
+    this._initBusqueda();
+  }
+
+  private _limpiarVehiculosSeleccionados() {
+    this._vehiculosIds = [];
+  }
+
+  private _initBusqueda() {
+    this.textoBusqueda.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((busqueda) => {
+          const parametrosConsulta = {
+            ...this._parametrosConsulta,
+            filtros: [
+              {
+                operador: '__icontains',
+                propiedad: 'placa__icontains',
+                valor1: busqueda,
+              },
+            ],
+          };
+
+          return this._vehiculoService
+            .lista(parametrosConsulta)
+            .pipe(map((response) => response.registros));
+        })
+      )
+      .subscribe((response) => {
+        this.vehiculosDisponibles$ = of(response);
+        this._limpiarVehiculosSeleccionados();
+        this.changeDetectorRef.detectChanges();
+      });
+  }
+
+  private _excluirVehiculosSeleccionados() {}
+
+  estoyEnListaEliminar(id: number): boolean {
+    return this._vehiculosIds.indexOf(id) !== -1;
+  }
+
+  manejarCheckItem(event: any, id: number) {
+    if (event.target.checked) {
+      this._agregarItemAListaEliminar(id);
+    } else {
+      this._removerItemDeListaEliminar(id);
+    }
+  }
+
+  private _agregarItemAListaEliminar(id: number) {
+    this._vehiculosIds.push(id);
+  }
+
+  private _removerItemDeListaEliminar(id: number) {
+    const itemsFiltrados = this._vehiculosIds.filter((item) => item !== id);
+    this._vehiculosIds = itemsFiltrados;
   }
 
   private _consultarVehiculos() {
@@ -76,9 +148,25 @@ export class AgregarFlotaComponent extends General implements OnInit {
     return this.formularioFlota.get('flota');
   }
 
-  enviar() {
-    console.log(this.flota.value);
+  get vehiculosIds() {
+    return this._vehiculosIds;
+  }
 
-    this._flotaService.agregarFlota(this.flota.value[0]).subscribe();
+  enviar() {
+    if (this._vehiculosIds.length) {
+      this._vehiculosIds.forEach((id) => {
+        this._flotaService.agregarFlota(id).subscribe();
+      });
+
+      this._dismissModal();
+      this.emitirConsultarLista.emit();
+    }
+  }
+
+  private _dismissModal() {
+    const modalEl: HTMLElement = document.querySelector('#agregar-flota');
+    const modal = KTModal.getInstance(modalEl);
+
+    modal.toggle();
   }
 }
