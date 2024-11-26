09a7b54df0401c37428b2ed42ab5167fa080557e
sebastian <sebastian.h.piedrahita@gmail.com>
2024-11-26 13:38:38 -0500

    feat: busqueda avanzada de filtros

--- src/app/modules/visita/mapeos/visita-rutear.mapeo.ts
+++ src/app/modules/visita/mapeos/visita-rutear.mapeo.ts
@@ -11,5 +11,15 @@ export const visitaRutearMapeo: any = {
       visibleFiltro: true,
       ordenable: true,
     },
+    {
+      nombre: 'franja__id',
+      campoNombre: 'Franja',
+      campoTipo: 'IntegerField',
+      visibleTabla: true,
+      visibleFiltro: true,
+      ordenable: true,
+      esFk: true,
+      modeloFk: 'RutFranja',
+    },
   ],
 };
