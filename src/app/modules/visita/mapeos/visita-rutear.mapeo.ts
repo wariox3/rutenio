21b2c88b3bff2746cc43703287d20e0f5a1aa162
sebastian <sebastian.h.piedrahita@gmail.com>
2025-03-10 16:59:38 -0500

    hotfix

--- src/app/modules/visita/mapeos/visita-rutear.mapeo.ts
+++ src/app/modules/visita/mapeos/visita-rutear.mapeo.ts
@@ -30,6 +30,14 @@ export const visitaRutearMapeo: any = {
       visibleFiltro: true,
       ordenable: false,
     },
+    // {
+    //   nombre: 'franja_codigo',
+    //   campoNombre: 'Franja código',
+    //   campoTipo: 'CharField',
+    //   visibleTabla: true,
+    //   visibleFiltro: true,
+    //   ordenable: false,
+    // },
     {
       nombre: 'estado_decodificado_alerta',
       campoNombre: 'Alerta',
