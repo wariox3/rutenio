decf10129e64807eb33b901952d38b8be5ea8277
sebastian <sebastian.h.piedrahita@gmail.com>
2025-04-21 14:39:12 -0500

    feat: esconder captcha

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -35,38 +35,23 @@ import { NgxTurnstileModule } from 'ngx-turnstile';
     InputEmailComponent,
     InputPasswordComponent,
     RouterLink,
-    NgxTurnstileModule
+    NgxTurnstileModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class RegisterComponent extends General {
+export default class RegisterComponent extends General implements OnInit {
   private authService = inject(AuthService);
   private _router = inject(Router);
   public registrando$ = new BehaviorSubject<boolean>(false);
   turnstileToken: string = '';
   turnstileSiteKey: string = environment.turnstileSiteKey;
+  isProduction: boolean = environment.production;
   public isLoading$ = new BehaviorSubject<boolean>(false);
 
-  validarContrasena(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const clave = control.root.get('password')?.value;
-      const confirmarClave = control.value;
-
-      return clave === confirmarClave ? null : { clavesDiferentes: true };
-    };
-  }
-
-  onTurnstileSuccess(token: string): void {
-    this.turnstileToken = token;
-    this.formulario.get('turnstileToken')?.setValue(token);
-    this.changeDetectorRef.detectChanges();
-  }
-
-
   formulario = new FormGroup({
-    turnstileToken: new FormControl('', [Validators.required]),
+    turnstileToken: new FormControl(''),
     username: new FormControl('', Validators.required),
     password: new FormControl(
       '',
@@ -85,6 +70,29 @@ export default class RegisterComponent extends General {
       Validators.compose([Validators.requiredTrue])
     ),
   });
+
+  ngOnInit(): void {
+    if (this.isProduction) {
+      this.formulario
+        .get('cf_turnstile_response')
+        ?.addValidators([Validators.required]);
+    }
+  }
+
+  validarContrasena(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const clave = control.root.get('password')?.value;
+      const confirmarClave = control.value;
+
+      return clave === confirmarClave ? null : { clavesDiferentes: true };
+    };
+  }
+
+  onTurnstileSuccess(token: string): void {
+    this.turnstileToken = token;
+    this.formulario.get('turnstileToken')?.setValue(token);
+    this.changeDetectorRef.detectChanges();
+  }
 
   enviar() {
     this.registrando$.next(true);
