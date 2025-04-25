c80e924e3779e2c4fa61706bb653e40e598b5650
sebas957 <baxtian957@hotmail.com>
2025-04-25 14:56:27 -0500

    filtrado de buscaque en adicionar visita pendiente

---
+++ src/app/modules/visita/mapeos/visita-adicionar-mapeo.ts
@@ -0,0 +1,40 @@
+export const visitaAdicionarMapeo: any = {
+  modulo: 'general',
+  modelo: 'guia',
+  tipo: 'Administrador',
+  datos: [
+    {
+      nombre: 'id',
+      campoNombre: 'Id',
+      campoTipo: 'IntegerField',
+      visibleTabla: true,
+      visibleFiltro: true,
+      ordenable: true,
+    },
+    {
+      nombre: 'numero',
+      campoNombre: 'Número',
+      campoTipo: 'IntegerField',
+      visibleTabla: true,
+      visibleFiltro: true,
+      ordenable: true,
+    },
+    {
+      nombre: 'destinatario',
+      campoNombre: 'Destinatario',
+      campoTipo: 'CharField',
+      visibleTabla: true,
+      visibleFiltro: true,
+      ordenable: true,
+    },
+
+    {
+      nombre: 'documento',
+      campoNombre: 'Documento',
+      campoTipo: 'CharField',
+      visibleTabla: true,
+      visibleFiltro: true,
+      ordenable: true,
+    },
+  ],
+};
