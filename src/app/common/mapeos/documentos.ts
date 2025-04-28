af438cccbe90f56c353c57495567edc436084256
sebas957 <baxtian957@hotmail.com>
2025-04-28 09:37:02 -0500

    Se retira campo liberadas en la lista de despachos

--- src/app/common/mapeos/documentos.ts
+++ src/app/common/mapeos/documentos.ts
@@ -172,14 +172,6 @@ export const mapeo: any = {
         ordenable: true,
       },
       {
-        encabezado: 'Liberadas',
-        campoNombre: 'visitas_liberadas',
-        campoTipo: 'CharField',
-        visibleTabla: true,
-        visibleFiltro: true,
-        ordenable: true,
-      },
-      {
         encabezado: 'Aprobado',
         campoNombre: 'estado_aprobado',
         campoTipo: 'Boolean',
