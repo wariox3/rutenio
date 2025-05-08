4244ceaaf7bc9b6b0890ede6424f10bcff7dc335
sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-08 17:17:34 -0500

    feat: color en los marcadores

--- src/app/modules/trafico/componentes/trafico-lista/trafico-lista.component.ts
+++ src/app/modules/trafico/componentes/trafico-lista/trafico-lista.component.ts
@@ -444,9 +444,9 @@ export default class TraficoListaComponent
       position,
       label: {
         text: label,
-        color: '#000000',
+        color: 'white',
         fontWeight: 'bold',
-        fontSize: '12px',
+        fontSize: '14px',
       },
       title: `Marker ${label}`,
       infoContent: {
@@ -571,5 +571,16 @@ export default class TraficoListaComponent
     const modal = KTModal.getInstance(modalEl);
 
     modal.toggle();
+  }
+
+  getMarkerIcon(color: string) {
+    return {
+      url: `data:image/svg+xml;base64,${btoa(`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${color}">
+          <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+        </svg>
+      `)}`,
+      scaledSize: new google.maps.Size(30, 30) // Ajusta el tamaño
+    };
   }
 }
