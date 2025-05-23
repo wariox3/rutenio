49a4a913f533c7f5784e462c145b5fb59d31bacd
sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-23 11:05:55 -0500

    feat: configuracion

---
+++ src/app/modules/configuracion/types/configuracion.types.ts
@@ -0,0 +1,15 @@
+export interface Configuracion {
+    id: number
+    empresa: number
+    informacion_factura: any
+    informacion_factura_superior: any
+    gen_uvt: string
+    gen_emitir_automaticamente: boolean
+    hum_factor: string
+    hum_salario_minimo: string
+    hum_auxilio_transporte: string
+    hum_entidad_riesgo: any
+    pos_documento_tipo: number
+    rut_sincronizar_complemento: boolean
+}
+  
