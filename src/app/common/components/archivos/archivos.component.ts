b53614f9b85db8d6ca360b7da67fb6e39acfc155
sebas957 <baxtian957@hotmail.com>
2025-06-13 15:15:01 -0500

    Ajustes cambios a componente de archivos

--- src/app/common/components/archivos/archivos.component.ts
+++ src/app/common/components/archivos/archivos.component.ts
@@ -9,11 +9,12 @@ import {
 import { GeneralService } from '../../services/general.service';
 import { ParametrosConsulta } from '../../../interfaces/general/api.interface';
 import { ArchivosService } from '../../services/archivos.service';
+import { FormatFechaPipe } from '../../pipes/formatear_fecha';
 
 @Component({
   selector: 'app-archivos',
   standalone: true,
-  imports: [],
+  imports: [FormatFechaPipe],
   templateUrl: './archivos.component.html',
   styleUrl: './archivos.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -60,4 +61,16 @@ export class ArchivosComponent implements OnInit {
       id: archivo.id,
     });
   }
+
+formatFileSize(bytes: number, decimals: number = 2): string {
+  if (bytes === 0) return '0 MB';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
 }
