ef0cc636290477ab054fb571f562de79dfc307e2
afcanop <afcanop@gmail.com>
2025-08-01 13:10:03 -0500

    feat: Implementar soporte para selección múltiple en filtros de relación y actualizar la lógica de condiciones

--- src/app/core/constants/filter/operadores-filtro.constant.ts
+++ src/app/core/constants/filter/operadores-filtro.constant.ts
@@ -5,7 +5,7 @@ export const OPERADORES_FILTRO: Operator[] = [
   {
     symbol: '=',
     name: 'Igual',
-    types: ['string', 'number', 'date', 'boolean' , 'relation'],
+    types: ['string', 'number', 'date', 'boolean', 'relation'],
     default: true,
   },
   {
@@ -31,5 +31,11 @@ export const OPERADORES_FILTRO: Operator[] = [
     name: 'Menor o igual a',
     types: ['number', 'date'],
     default: false,
+  },
+  {
+    symbol: 'in',
+    name: 'En lista',
+    types: ['relation'],
+    default: false, // ⚠️ No debe ser default si ya tienes '=' como default
   },
 ];
