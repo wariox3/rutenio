6b52d84eda7e96dfe90cd573d145674bfae75609
sebas957 <baxtian957@hotmail.com>
2025-08-11 18:12:49 -0500

    Ajustes filtrado por despacho

--- src/app/modules/despacho/mapeos/despacho-lista-mapeo.ts
+++ src/app/modules/despacho/mapeos/despacho-lista-mapeo.ts
@@ -1,7 +1,7 @@
 import { FilterField } from "../../../core/interfaces/filtro.interface";
 
 export const DESPACHO_LISTA_FILTERS: FilterField[] = [
-  { name: 'ID', displayName: 'id', type: 'number' },
+  { name: 'id', displayName: 'id', type: 'number' },
   { name: 'vehiculo__placa', displayName: 'Placa', type: 'string' },
   { name: 'estado_aprobado', displayName: 'Aprobado', type: 'boolean' },
   { name: 'estado_terminado', displayName: 'Terminado', type: 'boolean' },
