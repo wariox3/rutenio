66b85468c14716e6237ed5d3bb849f66c440c78b
sebas957 <baxtian957@hotmail.com>
2025-12-02 10:32:29 -0500

    filtros en trafico lista

---
+++ src/app/modules/trafico/interfaces/trafico.interface.ts
@@ -0,0 +1,39 @@
+export interface Trafico {
+  id: number;
+  guia: number;
+  fecha: string;
+  documento: string;
+  destinatario: string;
+  destinatario_direccion: string;
+  ciudad_id: number;
+  destinatario_telefono: string;
+  destinatario_correo: any;
+  unidades: number;
+  peso: number;
+  volumen: number;
+  cobro: number;
+  estado_decodificado: boolean;
+  latitud: number;
+  estado_entregado: boolean;
+  longitud: number;
+  orden: number;
+  distancia_proxima: number;
+  franja_id: number;
+  franja_codigo: any;
+  franja_nombre: string;
+  numero: number;
+  tiempo: number;
+  tiempo_servicio: number;
+  tiempo_trayecto: number;
+  estado_decodificado_alerta: boolean;
+  estado_despacho: boolean;
+  estado_novedad: boolean;
+  destinatario_direccion_formato: string;
+  resultados: any[];
+  datos_entrega: {
+    recibe: string;
+    recibeCelular: string;
+    recibeParentesco: string;
+    recibeNumeroIdentificacion: string;
+  };
+}
