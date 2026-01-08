948d3173d77e03fd0861dd996df3b08c3f566c63
Sebastian <sebastian.h.piedrahita@gmail.com>
2026-01-08 10:26:06 -0500

    Revert "feat: implementar gestión de estado de autenticación con signals y inicializador de aplicación"
    This reverts commit 2b440882655ea7b7010d9804e90192d976fee616.

--- src/app/common/guards/auth.guard.ts
+++ src/app/common/guards/auth.guard.ts
@@ -1,15 +1,16 @@
 import { inject } from '@angular/core';
 import { Router, type CanMatchFn } from '@angular/router';
 import { TokenService } from '../../modules/auth/services/token.service';
-import { AuthState } from '../../modules/auth/state/auth.state';
 
 export const authGuard: CanMatchFn = (route, segments) => {
-  const state = inject(AuthState);
+  const tokenValido = inject(TokenService).validarToken();
+
   const router = inject(Router);
 
-  if (state.isAuthenticated()) {
+  if (tokenValido) {
     return true;
   }
 
-  return router.parseUrl('/auth/login');
+  router.navigate(['/auth']);
+  return false;
 };
