43a8a667414589fcd5c2d6b7a0e002ce443a2579
Sebastian <sebastian.h.piedrahita@gmail.com>
2026-01-20 09:31:39 -0500

    feat: capitalizar displayName de "id" a "Id" en filtros de tab de tráfico de visitas
    - Cambiar "id" a "Id" en filtro id de VISITA_TRAFICO_TAB_FILTERS

--- src/app/modules/visita/mapeos/visita-trafico-tab-mapeo.ts
+++ src/app/modules/visita/mapeos/visita-trafico-tab-mapeo.ts
@@ -1,7 +1,7 @@
 import { FilterField } from "../../../core/interfaces/filtro.interface";
 
 export const VISITA_TRAFICO_TAB_FILTERS: FilterField[] = [
-  { name: 'id', displayName: 'id', type: 'number' },
+  { name: 'id', displayName: 'Id', type: 'number' },
   { name: 'numero', displayName: 'Número', type: 'number' },
   { name: 'estado_entregado', displayName: 'Entregado', type: 'boolean' },
   { name: 'estado_novedad', displayName: 'Novedad', type: 'boolean' },
