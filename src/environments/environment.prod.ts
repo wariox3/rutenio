d31fee2eb7a19d51193afc270d6147bd4bd83b2d
Ottoniel Hernandez <ottoniek14@gmail.com>
2026-04-28 14:44:54 -0500

    fix: apuntar environment.prod a http porque https rechaza conexion en server

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  url_api: 'http://ruteoapi.online',
+  url_api_subdominio: 'http://subdominio.ruteoapi.online',
+  dominioApp: '.ruteoapi.online',
+  googleMapsApiKey: 'AIzaSyCBn50oOWLjGY8VOXKOW7wJmGGXqSLxbO8',
+  dominioHttp: 'http',
+  llavePublica: 'pub_test_HrxfoMdxFQFlRQ5be2n0jplrqpAViOKb',
+  sessionLifeTime: 24,
+  turnstileSiteKey: '0x4AAAAAABL-n8pCTJYbGk6f',
+  url_reddoc_cuenta: 'http://cuenta.reddoc.online',
+  digitalOceanUrl: 'https://semanticadesarrollo.tor1.digitaloceanspaces.com/',
+  enableTurnstile: false,
+};
